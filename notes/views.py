b6a972f4999225d70f37c57ad8b7bd1f77f4e4a0
ToledoVitor <usedbr@gmail.com>
2023-02-08 22:06:22 -0300

    class based views

--- notes/views.py
+++ notes/views.py
@@ -1,18 +1,12 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.views.generic import DetailView, ListView
 
 from .models import Notes
 
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
 
-
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404("Note does not exist")
-
-    return render(request, 'notes/notes_detail.html', {'note': note})
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
